0f36432b8fd37a390720d6d069b6115a8f4b9c4e
gabriel-b-vitareli <gabriel.braga.vitareli23@gmail.com>
2026-09-03 16:31:31 -0300

    update

--- Reforço de Lógica/EX03.php
+++ Reforço de Lógica/EX03.php
@@ -3,7 +3,7 @@
 $dividendo = readline("Digite o dividendo: "); 
 $divisor = readline("Digite o disivor: ");
 
-if ($dividendo % $divisor = 0){ // Se a sobra da divisão for 0, então são divisíveis:
+if ($dividendo % $divisor == 0){ // Se a sobra da divisão for 0, então são divisíveis:
     echo "\033[;32mEsses números são divisíveis.\033[m";
 } elseif ($divisor == 0){ // Se o divisor for 0, evitamos um erro ao lembrar que nenhum número é divisível por 0:
     echo "\033[;31mNão é possível dividir por 0. \033[m";
