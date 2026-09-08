48756a289b78a166ce8a9dde2b4e439f1b19bc95
gabriel-b-vitareli <gabriel.braga.vitareli23@gmail.com>
2026-09-08 08:54:14 -0300

    Correção exercício 5

--- Atividades/Reforço de Lógica/Python/EX05.py
+++ Atividades/Reforço de Lógica/Python/EX05.py
@@ -1,8 +1,10 @@
 # ------------------- Verifica se um aluno passou na média ------------------- #
 
-nota = int(input("Digite sua nota: ")) # Pedindo a nota
+nota = float(input("Digite sua nota: ")) # Pedindo a nota
 
-if nota >= 7: # Se a nota for maior ou igual a 7, ele passou na média
+if nota >= 7 and nota < 10: # Se a nota for maior ou igual a 7 E menor que 10, ele passou na média
     print(f"\033[32mVocê passou na média com sua nota {nota}.\033[m")
-else: # Se não, ele reprovou
+elif nota > 0 and nota < 7: # Se não, se a nota dele for maior que zero (mas menor que 7), ele reprovou
     print(f"\033[31mVocê foi reprovado com sua nota {nota}.\033[m")
+else: # Se não, a única opção que sobra é que sua nota é um número inválido, como negativo ou maior que 10
+    print(f"\033[31mSua nota {nota} é inválida.\033[m") 
