d75b1ed81cc31130854619da12d1bd55490ccc3e
gabriel-b-vitareli <gabriel.braga.vitareli23@gmail.com>
2026-09-08 08:40:31 -0300

    Correção exercício 3

--- Reforço de Lógica/Python/EX03.py
+++ Reforço de Lógica/Python/EX03.py
@@ -3,12 +3,12 @@
 dividendo = int(input("Digite o dividendo: ")) # Pedindo o dividendo da operação
 divisor = int(input("Digite o disivor: ")) # Pedindo o divisor da operação
 
-# Se a divisão resultar em 0 sobra, eles são divisíveis:
-if dividendo % divisor == 0:
-    print("\033[;32mEsses números são divisíveis.\033[m")
 # Se o divisor for 0, avisamos que é impossível dividir por zero:
-elif divisor == 0:
+if divisor == 0:
     print("\033[;31mNão é possível dividir por 0. \033[m")
+# Se a divisão resultar em 0 sobra, eles são divisíveis:
+elif dividendo % divisor == 0:
+    print(f"\033[;32mEsses números são divisíveis. O resultado dessa divisão é {int(dividendo/divisor)}.\033[m")
 # Se não, só sobra a possibilidade dos números não serem divisíveis:
 else:
     print("\033[;31mEsses números não são divisíveis.\033[m")
