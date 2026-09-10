f196cc454808ec6772850c6940cbcf124ceb956e
gabriel-b-vitareli <gabriel.braga.vitareli23@gmail.com>
2026-09-10 14:05:02 -0300

    Criando index

---
+++ Desafios/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafios PHP</title>
+</head>
+<body>
+    <h1>Lista de desafios de PHP</h1>
+    <button><a href="/desafio-01.php">Desafio 01</a></button><br><br>
+    <button><a href="/desafio-02.php">Desafio 02</a></button><br><br>
+    <button><a href="/desafio-03.php">Desafio 03</a></button><br><br>
+    <button><a href="/desafio-04.php">Desafio 04</a></button><br><br>
+    <button><a href="/desafio-05.php">Desafio 05</a></button><br><br>
+
+    <footer>
+        <hr>
+        <small>Desafios solucionados por Gabriel Braga Vitareli&copy;</small>
+    </footer>
+</body>
+</html>
