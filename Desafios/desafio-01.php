0d5b19238f1ac031f4d294467a4408ab8f3b3bef
gabriel-b-vitareli <gabriel.braga.vitareli23@gmail.com>
2026-09-10 14:04:25 -0300

    Corrigindo desafio 1

--- Desafios/desafio-01.php
+++ Desafios/desafio-01.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio 01 | Número Antecessor e Sucessor</title>
 </head>
 <body>
+    <!-- Formulário para receber o número escolhido pelo usuário: -->
     <form action="" method="POST">
     <label for="numero">Digite um número:<br></label>
     <input type="number" name="numero" id="numero">
@@ -13,11 +14,15 @@
     </form>
 
     <?php 
+    // Quando o número do usuário for enviado/setado, ele executa o código:
     if(isset($_POST['numero'])){
-        $numero = $_POST['numero'];
-        echo "<h3>Número Selecionado: $numero</h3><hr>";
-        echo "<strong>Antecessor: </strong>" .$numero -1 ."<br>";
-        echo "<strong>Sucessor: </strong>" .$numero +1;
+        $numero = $_POST['numero']; // Guarda o número enviado pelo usuário na variável numero, apenas por organização de código
+
+        echo "<h3>Número Selecionado: $numero</h3><hr>"; // Printa o próprio número escolhido pelo usuário
+
+        echo "<strong>Antecessor: </strong>" .$numero -1 ."<br>"; // Printa o número - 1 (ou seja, antecessor)
+
+        echo "<strong>Sucessor: </strong>" .$numero +1; // Printa o número + 1 (ou seja, sucessor)
     }
     ?>
 
